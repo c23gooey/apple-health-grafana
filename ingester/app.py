73c7a1d707321a93f052814e4d3ed365bbc58760
Chris Adkins <c23gooey@gmail.com>
2023-02-27 22:12:53 +1100

    remove workout routes

--- ingester/app.py
+++ ingester/app.py
@@ -8,17 +8,13 @@ import time
 import xml.etree.ElementTree as etree
 from shutil import unpack_archive
 
-import gpxpy
 from dateutil.parser import parse
-from gpxpy.gpx import GPXTrackPoint
 from influxdb import InfluxDBClient
 
 client = InfluxDBClient('influx', 8086, database='health')
 PREFIX_RE = re.compile('HK.*Identifier(.+)$')
-zip_path = '/export.zip'
-unzip_path = "/export"
-routes_path = "/export/apple_health_export/workout-routes/"
-export_path = "/export/apple_health_export/export.xml"
+export_path = "/export.xml"
+
 
 def try_to_float(v):
     """convert v to float or 0"""
@@ -31,68 +27,24 @@ def try_to_float(v):
             return 0
 
 
-def format_route_point(name,point: GPXTrackPoint,next_point=None)-> dict:
-    """for a given `point`, creates an influxdb point
-    and computes speed and distance if `next_point` exists
-    """
-    slug_name = name.replace(" ","-").replace(":","-").lower()
-    datapoint = {
-        "measurement":'workout-routes',
-        "tags":{
-            "workout":slug_name
-        },
-        "time":point.time,
-        "fields":{
-            "latitude":point.latitude,
-            "longitude": point.longitude,
-            "elevation": point.elevation,
-            
-        }}
-    if next_point:
-        datapoint['fields']['speed'] = point.speed_between(next_point) if next_point else 0
-        datapoint['fields']['distance'] = point.distance_3d(next_point)
-    return datapoint
-
 def format_record(record):
     """format a export health xml record for influx"""
     m = re.match(PREFIX_RE, record.get("type"))
-    measurement =  m.group(1) if m else record.get("type")
-    value = try_to_float(record.get("value",1))
-    unit = record.get("unit","unit")
+    measurement = m.group(1) if m else record.get("type")
+    value = try_to_float(record.get("value", 1))
+    unit = record.get("unit", "unit")
     date = int(parse(record.get("startDate")).timestamp())
 
     return {
-        "measurement":measurement,
-        "tags":{
-            "unit":unit
+        "measurement": measurement,
+        "tags": {
+            "unit": unit
         },
-        "time":date,
-        "fields":{
-            "value":value
+        "time": date,
+        "fields": {
+            "value": value
         }
     }
-
-
-
-def parse_workout_route(route_xml_file):
-    with open(route_xml_file, 'r') as gpx_file:
-        gpx = gpxpy.parse(gpx_file)
-        for track in gpx.tracks:
-            track_points=[]
-            print("opening",track.name)
-            for segment in track.segments:
-                num_points = len(segment.points)
-                for i in range(num_points):
-                    track_points.append(format_route_point(track.name,segment.points[i],segment.points[i+1] if i +1 < num_points else None))
-            client.write_points(track_points,time_precision="s")
-
-
-def process_workout_routes():
-    print('loading workout routes')
-    for file in os.listdir(routes_path):
-        if file.endswith(".gpx"):
-            route_file = os.path.join(routes_path, file)
-            parse_workout_route(route_file)
 
 
 def process_health_data():
@@ -107,25 +59,17 @@ def process_health_data():
             # batch push every 10000
             if len(formatted_records) == 10000:
                 total_count += 10000
-                client.write_points(formatted_records,time_precision="s")
+                client.write_points(formatted_records, time_precision="s")
                 del formatted_records
                 formatted_records = []
-                print("inserted",total_count,"records")
+                print("inserted", total_count, "records")
 
     # push the rest
-    client.write_points(formatted_records,time_precision="s")
-    print("Total number of records:",total_count+len(formatted_records))
+    client.write_points(formatted_records, time_precision="s")
+    print("Total number of records:", total_count+len(formatted_records))
+
 
 if __name__ == "__main__":
-    print('unzipping the export file..')
-    try:
-        unpack_archive(zip_path, unzip_path)
-    except Exception as unzip_err:
-        print("Unable to open export zip: ",unzip_err)
-        exit(1)
-    print('export file unzipped')
-
-
     while True:
         try:
             client.ping()
@@ -137,6 +81,5 @@ if __name__ == "__main__":
             print("waiting on influx to be ready..")
             time.sleep(1)
 
-    process_workout_routes()
     process_health_data()
     print('All done ! You can now check grafana.')
